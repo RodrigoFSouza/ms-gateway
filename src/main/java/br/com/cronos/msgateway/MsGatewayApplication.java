3d396ad55dfdd28f6118baee72e39ed57941de0b
Rodrigo Souza <roferreira122@gmail.com>
2024-03-31 17:29:51 -0300

    corrigindo incluindo anotação de bean em routes e incluindo rota de cards

--- src/main/java/br/com/cronos/msgateway/MsGatewayApplication.java
+++ src/main/java/br/com/cronos/msgateway/MsGatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -14,9 +15,11 @@ public class MsGatewayApplication {
 		SpringApplication.run(MsGatewayApplication.class, args);
 	}
 
+	@Bean
 	public RouteLocator routes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.route(r -> r.path("/cards/**").uri("lb://mscards"))
 				.build();
 	}
 
